488d76dade2d29b136c373e4182b6424f545f3f8
Alex Tang <altitude@funkware.com>
2017-02-07 13:18:16 -0800

    Move reload to separate update block
    https://github.com/mcudich/HeckelDiff/issues/9
    
    reloadItems is done separately as the update indexes returne by diff() are in respect to the "after" state, but the collectionView.reloadItems() call wants the "before" indexPaths.

--- Source/UITableView+Diff.swift
+++ Source/UITableView+Diff.swift
@@ -23,11 +23,17 @@ public extension UITableView {
 
     deleteRows(at: update.deletions, with: animation)
     insertRows(at: update.insertions, with: animation)
-    reloadRows(at: update.updates, with: animation)
     for move in update.moves {
       moveRow(at: move.from, to: move.to)
     }
-
     endUpdates()
+
+    // reloadItems is done separately as the update indexes returne by diff() are in respect to the
+    // "after" state, but the collectionView.reloadItems() call wants the "before" indexPaths.
+    if update.updates.count > 0 {
+      beginUpdates()
+      reloadRows(at: update.updates, with: animation)
+      endUpdates()
+    }
   }
 }
