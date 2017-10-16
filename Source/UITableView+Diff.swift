29238c8dad0aca4fb3c2e002ecfeb086ada7a1ac
Leo Mehlig <leo@leomehlig.com>
2017-10-16 16:43:57 +0200

    Adds parameter to UIKit method to skip the reload of updated cells.

--- Source/UITableView+Diff.swift
+++ Source/UITableView+Diff.swift
@@ -13,11 +13,12 @@ import UIKit
 public extension UITableView {
   /// Applies a batch update to the receiver, efficiently reporting changes between old and new.
   ///
-  /// - parameter old:       The previous state of the table view.
-  /// - parameter new:       The current state of the table view.
-  /// - parameter section:   The section where these changes took place.
-  /// - parameter animation: The animation type.
-  func applyDiff<T: Collection>(_ old: T, _ new: T, inSection section: Int, withAnimation animation: UITableViewRowAnimation) where T.Iterator.Element: Hashable, T.IndexDistance == Int, T.Index == Int {
+  /// - parameter old:              The previous state of the table view.
+  /// - parameter new:              The current state of the table view.
+  /// - parameter section:          The section where these changes took place.
+  /// - parameter animation:        The animation type.
+  /// - parameter reloadUpdated:    Whether or not updated cells should be reloaded (default: true)
+  func applyDiff<T: Collection>(_ old: T, _ new: T, inSection section: Int, withAnimation animation: UITableViewRowAnimation, reloadUpdated: Bool = true) where T.Iterator.Element: Hashable, T.IndexDistance == Int, T.Index == Int {
     let update = ListUpdate(diff(old, new), section)
 
     beginUpdates()
@@ -31,7 +32,7 @@ public extension UITableView {
 
     // reloadItems is done separately as the update indexes returne by diff() are in respect to the
     // "after" state, but the collectionView.reloadItems() call wants the "before" indexPaths.
-    if update.updates.count > 0 {
+    if reloadUpdated && update.updates.count > 0 {
       beginUpdates()
       reloadRows(at: update.updates, with: animation)
       endUpdates()
