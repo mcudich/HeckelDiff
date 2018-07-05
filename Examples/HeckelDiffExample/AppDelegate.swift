d58e992d3ff0f38302d989159a1951342bb5e020
Emad Abdehagh <emad.abdehagh@foxsports.com.au>
2018-07-05 14:06:35 +1000

    Add supporting tvOS

---
+++ Examples/HeckelDiffExample/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HeckelDiffExample
+//
+//  Created by Matias Cudich on 11/23/16.
+//  Copyright © 2016 Matias Cudich. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow? = {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.backgroundColor = .white
+        return window
+    }()
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
